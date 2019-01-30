9b664439fb4ec20a258d0dc04fee1e0fc69555d0
Andreasp123 <38070600+Andreasp123@users.noreply.github.com>
2019-01-30 01:08:01 +0100

    Add files via upload

---
+++ Send email/file.php
@@ -0,0 +1,6 @@
+<?php
+
+
+$html = file_get_contents("file.html");
+echo $html;
+?>
---
+++ Send email/sent.php
@@ -0,0 +1,49 @@
+<?php
+require 'PHPMailerAutoload.php';
+$mail = new PHPMailer;
+
+$mail->IsSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 465;
+//465
+$mail->SMTPAuth = true;
+$mail->Username = 'persson.andreeas@gmail.com';
+$mail->Password = 'removed';
+$mail->SMTPSecure = 'ssl';
+$mail->SMTPOptions = array(
+  'ssl' => array(
+    'verify_peer' => false,
+    'verify_peer_name' => false,
+    'allow_self_signed' => true
+  )
+);
+
+	if(htmlspecialchars($_POST["password"]) == "1234"){
+		$recipient = htmlspecialchars($_POST['to']);
+		$tester = ($_POST["to"]);
+		
+		$mail->from = htmlspecialchars($_POST["from"]);
+		$mail->addAddress($recipient, 'name');
+		//htmlspecialchars($_POST['to']);
+		$mail->addCC = htmlspecialchars($_POST["cc"]);
+		$mail->addBCC = htmlspecialchars($_POST["bcc"]);
+		$mail->Subject = htmlspecialchars($_POST["subject"]);
+		$mail->Body = htmlspecialchars($_POST["message"]);
+		$mail->isHTML(true);
+	}
+	
+	if($mail->send()){
+		foreach($_POST as $key => $value){
+			if($key == "message"){
+				echo $value;
+			}
+			
+		}
+	}
+	else{
+		echo "Error: " . $mail->ErrorInfo;
+		
+	}
+	
+
+?>
